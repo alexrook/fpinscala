f288915eab0e0a24316a4b757ee9602b87c574d7
Александр Грачев <agrachev@neoflex.ru>
2024-08-07 16:38:12 +0300

    EXERCISE 6.2 implemented

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -51,7 +51,19 @@ object RNG {
     }
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  /** EXERCISE 6.2
+    *
+    * Write a function to generate a Double between 0 and 1, not including 1.
+    * Note: You can use Int.MaxValue to obtain the maximum positive integer
+    * value, and you can use x.toDouble to convert an x: Int to a Double.
+    */
+  def double(rng: RNG): (Double, RNG) = {
+    val (halfRet, newRNG) = nonNegativeInt(rng)
+    halfRet match {
+      case m @ Int.MaxValue => (m - 1).toDouble / m -> newRNG
+      case x                => x.toDouble / Int.MaxValue -> newRNG
+    }
+  }
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = ???
 
--- exercises/src/test/scala/fpinscala/state/StateTest.scala
+++ exercises/src/test/scala/fpinscala/state/StateTest.scala
@@ -32,6 +32,41 @@ class StateTest extends AnyWordSpec {
           val (ret, _) = nonNegativeInt(rng)
           assert(ret > -1)
         }
+
+      }
+
+      "return positive results for corner cases" in {
+        val rng1 = RNG.Simple(Int.MinValue)
+        val (ret1, _) = nonNegativeInt(rng1)
+        assert(ret1 > -1)
+
+        val rng2 = RNG.Simple(Int.MaxValue)
+        val (ret2, _) = nonNegativeInt(rng2)
+        assert(ret2 > -1)
+      }
+    }
+
+    "double method called " should {
+      "return values between 0 and 1 multiple times" in {
+        for (x <- -10 to 10) {
+          val rng = RNG.Simple(x)
+          val (ret, _) = double(rng)
+          assert(ret >= 0 && ret < 1)
+        }
+      }
+
+      "return correct results for corner cases" in {
+        val rng1 = RNG.Simple(Int.MinValue)
+        val (ret1, _) = double(rng1)
+        assert(ret1 >= 0 && ret1 < 1)
+
+        val rng2 = RNG.Simple(Int.MaxValue)
+        val (ret2, _) = double(rng2)
+        assert(ret2 >= 0 && ret2 < 1)
+
+        val rng3 = RNG.Simple(0)
+        val (ret3, _) = double(rng3)
+        assert(ret3 >= 0 && ret3 < 1)
       }
     }
   }
