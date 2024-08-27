ae55174b69335f60a34b8905508e42e78c965cbf
Александр Грачев <agrachev@neoflex.ru>
2024-08-27 15:10:25 +0300

    EXERCISE 11.1 implemented

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -8,49 +8,54 @@ import state._
 import parallelism.Par._
 import language.higherKinds
 
-
 trait Functor[F[_]] {
-  def map[A,B](fa: F[A])(f: A => B): F[B]
+  def map[A, B](fa: F[A])(f: A => B): F[B]
 
-  def distribute[A,B](fab: F[(A, B)]): (F[A], F[B]) =
+  /** We wrote this just by following the types, but let’s think about what it
+    * means for con- crete data types like List, Gen, Option, and so on. For
+    * example, if we distribute a List[(A, B)], we get two lists of the same
+    * length, one with all the As and the other with all the Bs. That operation
+    * is sometimes called unzip
+    */
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
     (map(fab)(_._1), map(fab)(_._2))
 
-  def codistribute[A,B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
-    case Left(fa) => map(fa)(Left(_))
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa)  => map(fa)(Left(_))
     case Right(fb) => map(fb)(Right(_))
   }
 }
 
 object Functor {
   val listFunctor = new Functor[List] {
-    def map[A,B](as: List[A])(f: A => B): List[B] = as map f
+    def map[A, B](as: List[A])(f: A => B): List[B] = as map f
   }
 }
 
 trait Monad[M[_]] extends Functor[M] {
   def unit[A](a: => A): M[A]
-  def flatMap[A,B](ma: M[A])(f: A => M[B]): M[B]
+  def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
 
-  def map[A,B](ma: M[A])(f: A => B): M[B] =
+  def map[A, B](ma: M[A])(f: A => B): M[B] =
     flatMap(ma)(a => unit(f(a)))
-  def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
+  def map2[A, B, C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 
   def sequence[A](lma: List[M[A]]): M[List[A]] = ???
 
-  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] = ???
+  def traverse[A, B](la: List[A])(f: A => M[B]): M[List[B]] = ???
 
   def replicateM[A](n: Int, ma: M[A]): M[List[A]] = ???
 
-  def compose[A,B,C](f: A => M[B], g: B => M[C]): A => M[C] = ???
+  def compose[A, B, C](f: A => M[B], g: B => M[C]): A => M[C] = ???
 
   // Implement in terms of `compose`:
-  def _flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = ???
+  def _flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] = ???
 
   def join[A](mma: M[M[A]]): M[A] = ???
 
   // Implement in terms of `join`:
-  def __flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = ???
+  def __flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] = ???
 }
 
 case class Reader[R, A](run: R => A)
@@ -58,19 +63,46 @@ case class Reader[R, A](run: R => A)
 object Monad {
   val genMonad = new Monad[Gen] {
     def unit[A](a: => A): Gen[A] = Gen.unit(a)
-    override def flatMap[A,B](ma: Gen[A])(f: A => Gen[B]): Gen[B] =
+    override def flatMap[A, B](ma: Gen[A])(f: A => Gen[B]): Gen[B] =
       ma flatMap f
   }
 
-  val parMonad: Monad[Par] = ???
+  /** EXERCISE 11.1
+    *
+    * Write monad instances for Par, Parser, Option, Stream, and List.
+    */
 
-  def parserMonad[P[+_]](p: Parsers[P]): Monad[P] = ???
+  val parMonad: Monad[Par] =
+    new Monad[Par] {
+      def flatMap[A, B](ma: Par[A])(f: A => Par[B]): Par[B] = Par.flatMap(ma)(f)
+      def unit[A](a: => A): Par[A] = Par.unit(a)
+    }
 
-  val optionMonad: Monad[Option] = ???
+  def parserMonad[P[+_]](p: Parsers[P]): Monad[P] = ??? // TODO:impl
 
-  val streamMonad: Monad[Stream] = ???
+  val optionMonad: Monad[Option] =
+    new Monad[Option] {
+      def flatMap[A, B](ma: Option[A])(f: A => Option[B]): Option[B] =
+        ma.flatMap(f)
 
-  val listMonad: Monad[List] = ???
+      def unit[A](a: => A): Option[A] = Option(a)
+
+    }
+
+  val streamMonad: Monad[laziness.Stream] =
+    new Monad[laziness.Stream] {
+      def flatMap[A, B](ma: laziness.Stream[A])(
+          f: A => laziness.Stream[B]
+      ): laziness.Stream[B] = ma.flatMap(f)
+      def unit[A](a: => A): laziness.Stream[A] = laziness.Stream.apply(a)
+
+    }
+
+  val listMonad: Monad[List] = 
+    new Monad[List] {
+      def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] = ma.flatMap(f)
+      def unit[A](a: => A): List[A] = List(a)
+    }
 
   def stateMonad[S] = ???
 
@@ -85,9 +117,10 @@ case class Id[A](value: A) {
 }
 
 object Reader {
-  def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
-    def unit[A](a: => A): Reader[R,A] = ???
-    override def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] = ???
+  def readerMonad[R] = new Monad[({ type f[x] = Reader[R, x] })#f] {
+    def unit[A](a: => A): Reader[R, A] = ???
+    override def flatMap[A, B](st: Reader[R, A])(
+        f: A => Reader[R, B]
+    ): Reader[R, B] = ???
   }
 }
-
